60f10b5b1cfb5ff09d6ccaa6c215763039af961c
WHD597312 <1352250152@qq.com>
2018-03-07 11:01:02 +0800

    这一改变的类

--- app/src/main/java/com/xinrui/draw/DrawOneActivity.java
+++ app/src/main/java/com/xinrui/draw/DrawOneActivity.java
@@ -3,14 +3,17 @@ package com.xinrui.draw;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Window;
 
 public class DrawOneActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_draw_one);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
 }
